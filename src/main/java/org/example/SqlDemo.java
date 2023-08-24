7668695cac58af68d3d994ce826ab12bc1e50909
诸葛子房 <xiaoqiu2017wy@163.com>
2023-08-24 15:13:28 +0800

    Update SqlDemo.java

--- src/main/java/org/example/SqlDemo.java
+++ src/main/java/org/example/SqlDemo.java
@@ -18,8 +18,17 @@ public class SqlDemo {
 
         TableEnvironment tableEnv = StreamTableEnvironment.create(env, settings);
 
-        //把kafka 中的topic映射成一个输入临时表
         tableEnv.executeSql(
+                "create table sensor_source (f_sequence INT,f_random_str STRING,ts AS localtimestamp,WATERMARK FOR ts AS ts) with (" +
+                        " 'connector' = 'datagen'," +
+                        "  'rows-per-second' = '5'," +
+                        "  'fields.f_random_str.length' = '10'," +
+                        "  'fields.f_sequence.min' = '1'," +
+                        "  'fields.f_sequence.max' = '500')"
+        );
+        
+        //把kafka 中的topic映射成一个输入临时表
+        /*tableEnv.executeSql(
                 "create table sensor_source (id string,name string) with (" +
                         " 'connector' = 'kafka'," +
                         "  'topic' = 'test_info_test'," +
@@ -27,7 +36,7 @@ public class SqlDemo {
                         "  'properties.group.id' = 'testGroup'," +
                         "  'scan.startup.mode' = 'earliest-offset'," +
                         "  'format' = 'json')"
-        );
+        );*/
         //把mysql 中的表映射成一个输出临时表
 
         String sql = "CREATE TABLE print_table (\n" +
@@ -38,22 +47,22 @@ public class SqlDemo {
                 ")";
 
         String mysql_sql = "CREATE TABLE mysql_sink (\n" +
-                "                   id string,\n" +
-                "                   name string\n" +
+                "                   f_sequence INT,\n" +
+                "                   f_random_str string\n" +
                 " ) WITH (\n" +
                 "   'connector' = 'jdbc',\n" +
                 "   'url' = 'jdbc:mysql://101.71.102.255:8081/kafka?serverTimezone=UTC',\n" +
-                "   'table-name' = 'test_info',\n" +
+                "   'table-name' = 'test',\n" +
                 "   'username' = 'kafka',\n" +
                 "   'password' = 'Bonc@123'\n" +
                 " )";
 
-        String kafka_sink_sql=
+        /*String kafka_sink_sql=
                 "create table kafka_sink (id string,name string) with (" +
                         " 'connector' = 'kafka'," +
                         "  'topic' = 'test_info_2'," +
                         "  'properties.bootstrap.servers' = 'localhost:9092'," +
-                        "  'format' = 'json')";
+                        "  'format' = 'json')";*/
 
         tableEnv.executeSql(mysql_sql);
         //tableEnv.executeSql(kafka_sink_sql);
